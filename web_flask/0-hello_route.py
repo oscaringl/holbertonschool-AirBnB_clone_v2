4ef86d9a47018b77e1de2c48a3e49303e26c6b6c
Oscar Guerra <oogl29@gmail.com>
2023-04-14 23:33:59 -0500

    Create a file 0-hello_route.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Starts Flask web application"""
+from flask import Flask
+app = Flask('web_flask')
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """Return string"""
+    return 'Hello HBNB!'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
